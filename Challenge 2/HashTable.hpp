685bb0e4898d720396eadf31bc4bef9594d12c9c
Skyryk <PatrykJWisniewski@gmail.com>
2019-03-19 17:18:50 -0600

    Revert "Revert "Merge branch 'master' of https://github.com/Skyryk/CSCI-2270""
    This reverts commit 0f47538108b5a7c4cdec10aba47a6d8ded38e1eb.

---
+++ Challenge 2/HashTable.hpp
@@ -0,0 +1,51 @@
+#ifndef HW_7_HASH_TABLE
+#define HW_7_HASH_TABLE
+
+#include <string>
+
+// struct to store word + count combinations
+struct wordItem
+{
+    std::string word;
+    int count;
+    wordItem* next;
+    wordItem* left;
+    wordItem* right;
+};
+
+/* class HashTable for storing words.
+ *   You will create two hash tables in your driver:
+ *      - one for storing stop words
+ *      - one for storing unique non-stop words
+ *   Why? We can load all the stopwords into the first table.
+ *   Then, we can quickly check that first hash table to see if
+ *   a word is a stopword before adding it to the second.
+ */
+class HashTable {
+  public:
+    HashTable(int hashTableSize);
+    ~HashTable();
+    void addWord(std::string word);
+    bool isInTable(std::string word);
+    void incrementCount(std::string word);
+    void printTopN(int n);
+    int getNumCollisions();
+    int getNumItems();
+    int getTotalNumWords();
+
+  private:
+    /* member functions */
+    unsigned int getHash(std::string word);
+    wordItem* searchTable(std::string word);
+    void PrintInOrder(wordItem* root, int n, int totalNumberOfWords);
+
+    /* instance variables */
+    wordItem* treeRoot;
+    int wordsPrinted;
+    wordItem** hashTable;
+    int hashTableSize;
+    int numItems;
+    int numCollisions;
+};
+
+#endif
